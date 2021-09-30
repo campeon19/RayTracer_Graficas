bd3d9a8c998d8bf6c34dc24aa336bf9672056fa3
Christian Pérez <christiandp@live.com>
2021-09-30 00:03:26 -0600

    Tarea 2 - Opaque, Reflections & Refraction

--- RayTracer.py
+++ RayTracer.py
@@ -3,9 +3,12 @@
 # Graficas por Computador 
 
 
-from gl import RayTracer, V3, _color
-from obj import Obj, Texture
-from figures import Sphere, Material
+from gl import Raytracer, V3
+from obj import Obj, Texture, EnvMap, _color
+from figures import REFLECTIVE, TRANSPARENT, PointLight, Sphere, Material, AmbientLight, DirectionalLight
+
+width = 1000
+height = 512
 
 # width = 300
 # height = 512
@@ -13,33 +16,61 @@ from figures import Sphere, Material
 # width = 600
 # height = 1024
 
-width = 1000
-height = 1424
+# width = 1000
+# height = 1424
 
-rtx = RayTracer(width, height)
+rtx = Raytracer(width, height)
 
-nieve = Material( diffuse = _color(0.9372,0.882,0.78))
-white = Material( diffuse = _color(1,1,1))
-black = Material( diffuse = _color(0,0,0))
-carrot = Material( diffuse = _color(0.93,0.57,0.13))
-stone = Material( diffuse = _color(0.5451,0.549,0.4784))
-rojo = Material( diffuse = _color(1,0,0))
 
-rtx.scene.append( Sphere(V3(0,-3,-10), 2.3, nieve) )
-rtx.scene.append( Sphere(V3(0,0.8,-11), 2, nieve) )
-rtx.scene.append( Sphere(V3(0,3.7,-10), 1.4, nieve) )
-rtx.scene.append( Sphere(V3(0,-2.5,-8), 0.5, black) )
-rtx.scene.append( Sphere(V3(0,-0.7,-8), 0.35, black) )
-rtx.scene.append( Sphere(V3(0,1,-8), 0.3, black) )
-rtx.scene.append( Sphere(V3(0,3,-8), 0.3, carrot) )
-rtx.scene.append( Sphere(V3(0.3,3.5,-8), 0.15, white) )
-rtx.scene.append( Sphere(V3(-0.3,3.5,-8), 0.15, white) )
-rtx.scene.append( Sphere(V3(0.27,3.1,-7), 0.06, black) )
-rtx.scene.append( Sphere(V3(-0.27,3.1,-7), 0.06, black) )
-rtx.scene.append( Sphere(V3(0.2,2.35,-8), 0.1, stone) )
-rtx.scene.append( Sphere(V3(-0.2,2.35,-8), 0.1, stone) )
-rtx.scene.append( Sphere(V3(-0.5,2.55,-8), 0.1, stone) )
-rtx.scene.append( Sphere(V3(0.5,2.55,-8), 0.1, stone) )
+# nieve = Material( diffuse = _color(0.9372,0.882,0.78))
+# white = Material( diffuse = _color(1,1,1))
+# black = Material( diffuse = _color(0,0,0))
+carrot = Material( diffuse = (0.93,0.57,0.13))
+stone = Material( diffuse = (0.5451,0.549,0.4784), spec=64)
+# rojo = Material( diffuse = _color(1,0,0))
+grass = Material( diffuse = (0.4,1,0), spec=128)
+brick = Material( diffuse= (0.8,0.25,0.25), spec=32)
+
+
+glass = Material(spec=64, ior = 1.5, matType=TRANSPARENT)
+glass2 = Material(spec=64, ior = 1.8, matType=TRANSPARENT)
+
+mirror = Material(spec=128,matType=REFLECTIVE)
+mirror2 = Material(diffuse=(0,0,1),spec=64,matType=REFLECTIVE)
+
+# rtx.scene.append( Sphere(V3(0,-3,-10), 2.3, nieve) )
+# rtx.scene.append( Sphere(V3(0,0.8,-11), 2, nieve) )
+# rtx.scene.append( Sphere(V3(0,3.7,-10), 1.4, nieve) )
+# rtx.scene.append( Sphere(V3(0,-2.5,-8), 0.5, black) )
+# rtx.scene.append( Sphere(V3(0,-0.7,-8), 0.35, black) )
+# rtx.scene.append( Sphere(V3(0,1,-8), 0.3, black) )
+# rtx.scene.append( Sphere(V3(0,3,-8), 0.3, carrot) )
+# rtx.scene.append( Sphere(V3(0.3,3.5,-8), 0.15, white) )
+# rtx.scene.append( Sphere(V3(-0.3,3.5,-8), 0.15, white) )
+# rtx.scene.append( Sphere(V3(0.27,3.1,-7), 0.06, black) )
+# rtx.scene.append( Sphere(V3(-0.27,3.1,-7), 0.06, black) )
+# rtx.scene.append( Sphere(V3(0.2,2.35,-8), 0.1, stone) )
+# rtx.scene.append( Sphere(V3(-0.2,2.35,-8), 0.1, stone) )
+# rtx.scene.append( Sphere(V3(-0.5,2.55,-8), 0.1, stone) )
+# rtx.scene.append( Sphere(V3(0.5,2.55,-8), 0.1, stone) )
+
+rtx.glClearColor(0.2,0.6,0.8)
+rtx.glClear()
+rtx.envmap = EnvMap('envmap2.bmp')
+
+rtx.ambLight = AmbientLight(strength= 0.1)
+rtx.dirLight = DirectionalLight(direction=V3(1,-1,-2), intensity=0.5) 
+rtx.pointLights.append(PointLight(position=V3(0,2,0), intensity=0.5))
+
+rtx.scene.append( Sphere(V3(-4,2,-8), 1, glass))
+rtx.scene.append( Sphere(V3(0,2,-8), 1, glass2))
+rtx.scene.append( Sphere(V3(4,2,-8), 1, mirror))
+rtx.scene.append( Sphere(V3(-4,-2,-8), 1, mirror2))
+rtx.scene.append( Sphere(V3(0,-2,-8), 1, carrot))
+rtx.scene.append( Sphere(V3(4,-2,-8), 1, grass))
+
+
+
 rtx.glRender()
 
-rtx.glFinish('Pruebas.bmp')
+rtx.glFinish('Tarea2.bmp')
--- figures.py
+++ figures.py
@@ -1,38 +1,71 @@
 # Christian Daniel Perez De Leon 19710
 
-from gl import WHITE
+from gl import V3
 import matematica as mate
+import numpy as np
+
+OPAQUE = 0
+REFLECTIVE = 1
+TRANSPARENT = 2
+
+WHITE = (1,1,1)
+
+class DirectionalLight(object):
+    def __init__(self, direction = V3(0,-1,0), intensity = 1, color = WHITE ):
+        self.direction = direction / np.linalg.norm(direction)
+        self.intensity = intensity
+        self.color = color
+
+class AmbientLight(object):
+    def __init__(self, strength = 0, color = WHITE):
+        self.strength = strength
+        self.color = color
+
+    def getColor(self):
+        return (self.strength * self.color[0] / 255,
+                self.strength * self.color[1] / 255,
+                self.strength * self.color[2] / 255)
+
+class PointLight(object):
+    # Luz con punto de origen que va en todas direcciones
+    def __init__(self, position = V3(0,0,0), intensity = 1, color = WHITE):
+        self.position = position
+        self.intensity = intensity
+        self.color = color
 
 class Material(object):
-    def __init__(self, diffuse = WHITE):
+    def __init__(self, diffuse = WHITE, spec = 1, ior = 1, matType = OPAQUE):
         self.diffuse = diffuse
+        self.spec = spec
+        self.matType = matType
+        self.ior = ior
 
 
 class Intersect(object):
-    def __init__(self, distance):
+    def __init__(self, distance, point, normal, sceneObject):
         self.distance = distance
+        self.point = point
+        self.normal = normal
+        self.sceneObject = sceneObject
 
 class Sphere(object):
-    def __init__(self, center, radius, material):
+    def __init__(self, center, radius, material = Material()):
         self.center = center
         self.radius = radius
         self.material = material
 
     def ray_intersect(self, orig, dir):
 
-        # P = O + t * D
+        L = np.subtract(self.center, orig)
+        l = np.linalg.norm(L)
 
-        L = mate.restaVect(self.center, orig)
-        l = mate.normVector(L)
+        tca = np.dot(L, dir)
 
-        tca = mate.productoPunto(L, dir)
-
-        d = (l**2 - tca**2) ** 0.5
-
-        if d > self.radius:
+        d = (l**2 - tca**2)
+        if d > self.radius ** 2:
             return None
 
-        thc = (self.radius**2 - d**2) ** 0.5
+        thc = (self.radius**2 - d) ** 0.5
         t0 = tca - thc
         t1 = tca + thc
 
@@ -42,4 +75,12 @@ class Sphere(object):
         if t0 < 0:
             return None
 
-        return Intersect( distance = t0 )
+        # P = O + t * D
+        hit = np.add(orig, t0 * np.array(dir) )
+        normal = np.subtract( hit, self.center )
+        normal = normal / np.linalg.norm(normal) #la normalizo
+
+        return Intersect( distance = t0,
+                          point = hit,
+                          normal = normal,
+                          sceneObject = self)
